611458269909bac1dcab38eacf2a48c9b4e30b84
Vijayalakshmi0012 <120468347+Vijayalakshmi0012@users.noreply.github.com>
2023-03-17 19:52:03 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,20 +1,28 @@
-import React from "react";
-import {ToastContainer,toast}from 'react-toastify'
-import'react-toastify/dist/ReactToastify.css';
-const App=()=>{
-  let handleSubmit=()=>{
-    toast.success("logged in successfully",
-    toast.success("looged off"),
-    {position:toast.POSITION.TOP_CENTER
-    })
-  }   
+import React,{ Component }from'react'
+export  default class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      username:"Appu"
+    }
+  }
+ componentDidMount(){
+
+    let btn=document.querySelector('button')
+    btn.addEventListener("click",()=>{
+      this.setState({username:this.state.username="Aryan"})
+    }
+    )
+  }
+render(){
   return(
+    <>
     <div>
-      <ToastContainer/>
-      <button onClick={handleSubmit}>
-        click for msg
-      </button>
+      <h1>{this.state.username}</h1>
     </div>
+    <button>change</button>
+    </>
   )
 }
-export default App
+
+}
